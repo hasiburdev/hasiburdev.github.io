a65014f9a321fcc11eeedc0830e91b13c7beea49
Hasibur Rahman <hasiburdev@gmail.com>
2023-07-12 23:40:17 +0600

    Update resume

--- data/home.ts
+++ data/home.ts
@@ -6,7 +6,7 @@ export const home: HomePageProps = {
   animatedText: "Programming is not just about coding, it is an Art",
   summary:
     "I'm a passionate Web Application and Software Developer specializing in MERN Stack Web Development. As a full-stack developer, I can convert Figma design to a perfect responsive web template and give them life using React Js. Also, I can make a mobile application. I can develop scalable REST APIs using Node Js, Express Js & Mongo DB and connect them with front-end applications, and also I'm experienced with SDLC and Agile methodology.",
-  resumeLink: "/resume.pdf",
+  resumeLink: "/Hasibur_Frontend_Developer.pdf",
   emailAddress: "hasiburdev@gmail.com",
   profileImage: profileImage,
 };
