642a878afb312024393f8b97baa4e9850dcd3a9a
Hasibur Rahman <hasiburdev@gmail.com>
2026-05-01 03:41:50 +0600

    fix: update tabs prop in DocsLayout to use an empty object

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -4,7 +4,7 @@ import { baseOptions } from "@/lib/layout.shared";
 
 export default function Layout({ children }: LayoutProps<"/docs">) {
   return (
-    <DocsLayout tree={source.getPageTree()} tabs={true} {...baseOptions()}>
+    <DocsLayout tree={source.getPageTree()} tabs={{}} {...baseOptions()}>
       {children}
     </DocsLayout>
   );
